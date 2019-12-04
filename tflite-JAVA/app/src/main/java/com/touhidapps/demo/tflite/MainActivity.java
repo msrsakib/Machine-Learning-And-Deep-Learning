3e5d78d0ad681bf70e77c20e10e9d95f269db197
msrsakib <sohansakib@gmail.com>
2019-12-05 04:13:47 +0600

    Added tflite and lebel

---
+++ tflite-JAVA/app/src/main/java/com/touhidapps/demo/tflite/MainActivity.java
@@ -0,0 +1,37 @@
+package com.touhidapps.demo.tflite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+  private Button mCameraBtn;
+  private Button mInfoBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mCameraBtn = (Button) findViewById(R.id.camera_btn);
+        mInfoBtn =(Button) findViewById(R.id.file_btn);
+
+        mCameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,CameraActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
